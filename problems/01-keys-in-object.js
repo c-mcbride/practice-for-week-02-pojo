5025bf611da8e024505aae55156bfc881eb2340e
c-mcbride <califfmcbride@gmail.com>
2023-12-20 22:55:43 -0600

    complete two exercises

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -1,5 +1,5 @@
 /***********************************************************************
-Write a function called `keysInObject(obj)` that takes in an object and returns 
+Write a function called `keysInObject(obj)` that takes in an object and returns
 an array of all the keys within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.keys`.
@@ -12,8 +12,14 @@ keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
-function keysInObject(obj) {
-  // Your code here
+function keysInObject(obj){
+  let keyArray = [];
+
+  for(let currentKey in obj){
+    keyArray.push(currentKey);
+  }
+
+  return keyArray; 
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
